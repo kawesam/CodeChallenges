73507fce5d0c32f06e139e2ac2f327410cbf8436
samlinncon <samlinncon@gmail.com>
2020-06-24 18:28:55 +0300

    structurally unique BST's (binary search trees) that store values 1 ... n?

---
+++ LeetCode/unique-binary-search-tree.js
@@ -0,0 +1,33 @@
+// Unique Binary Search Trees
+//
+// Solution
+// Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
+//
+// Example:
+//
+//     Input: 3
+// Output: 5
+// Explanation:
+//     Given n = 3, there are a total of 5 unique BST's:
+//
+//    1         3     3      2      1
+//     \       /     /      / \      \
+//      3     2     1      1   3      2
+//      /     /       \                 \
+//    2     1          2                 3
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function(n) {
+
+    let catalanNumber = 1;
+
+    for(let i =0 ;i< n;i++){
+        catalanNumber = catalanNumber * 2 * (2 * i + 1) / (i + 2);
+    }
+
+    return catalanNumber;
+};
