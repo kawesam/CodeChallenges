93c1d5e401e847886e19797b9419b028c2737bc6
samlinncon <samlinncon@gmail.com>
2020-06-25 21:16:11 +0300

    solution to get the missing duplicate number in an array

---
+++ LeetCode/find_duplicate_number.js
@@ -0,0 +1,36 @@
+// Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+//
+//     Example 1:
+//
+// Input: [1,3,4,2,2]
+// Output: 2
+// Example 2:
+//
+// Input: [3,1,3,4,2]
+// Output: 3
+// Note:
+//
+//    1: You must not modify the array (assume the array is read only).
+//    2: You must use only constant, O(1) extra space.
+//    3: Your runtime complexity should be less than O(n2).
+//    4:There is only one duplicate number in the array, but it could be repeated more than once.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    let duplicate = [];
+    let sortedArray = nums.sort(function(a, b){
+        return a - b;
+    });
+
+    for(let i = 0;i<sortedArray.length;i++){
+
+        if(sortedArray[i-1] == sortedArray[i]){
+            return sortedArray[i];
+        }
+    }
+
+
+};
