11ff50a2ec1919a3e852860a9dfba538841b324e
samlinncon <samlinncon@gmail.com>
2020-06-30 22:09:30 +0300

    get unique paths

---
+++ LeetCode/unique_paths.js
@@ -0,0 +1,48 @@
+// A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+//
+// The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+//
+// How many possible unique paths are there?
+//
+//
+//     Above is a 7 x 3 grid. How many possible unique paths are there?
+//
+//
+
+// Example 1:
+//
+// Input: m = 3, n = 2
+// Output: 3
+// Explanation:
+//     From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+//     1. Right -> Right -> Down
+// 2. Right -> Down -> Right
+// 3. Down -> Right -> Right
+// Example 2:
+//
+// Input: m = 7, n = 3
+// Output: 28
+//
+//
+// Constraints:
+//
+//     1 <= m, n <= 100
+// It's guaranteed that the answer will be less than or equal to 2 * 10 ^ 9.
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    const matrix = Array.from({ length: m }, () => Array(n).fill(1));
+
+    // console.log(matrix);
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1];
+        }
+    }
+    // console.log(matrix);
+    return matrix[m - 1][n - 1];
+};
